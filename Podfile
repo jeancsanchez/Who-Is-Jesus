2b99c949b7524ee63359c443ec68c5f68cfe0859
jeancsanchez <jeancs.patricio@gmail.com>
2017-12-30 18:38:41 -0300

    Prepare podfile for integrating with Firebase.

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'WhoIsJesus' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for WhoIsJesus
+pod 'Firebase/Core'
+pod 'Firebase/Messaging'
+
+  target 'WhoIsJesusTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'WhoIsJesusUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- WhoIsJesus/AppDelegate.swift
+++ WhoIsJesus/AppDelegate.swift
@@ -13,6 +13,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?)
+        -> Bool {
+            FirebaseApp.configure()
+            return true
+    }
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -40,7 +47,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
 
 }
 
